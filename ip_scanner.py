3cc3b8b258f3e2824104a14203a9e9fad1473eac
Derek Williams <Williamsdj503@gmail.com>
2025-01-31 10:47:28 -0700

    IP scanner

--- ip_scanner.py
+++ ip_scanner.py
@@ -19,7 +19,7 @@ def ping_host(ip):
     #Use the time module to get the exact time when we ping the host
     start_time = time.time()
 
-    #Since the ping command works differently on Windows and Linux if the system is Windows then go with the first option or we know it's Linux and go with the second option.
+    #Since the ping command works differently on Windows and Linux we use the sys module to check if the system is Windows and if it is then go with the first option otherwise we know it's Linux and go with the second option.
     if sys.platform == "win32":
 
         #'ping' is the actual command we want to run and '-n' is the amount of packets we want to send which is '1' and then '-w' is the timeout which is '1000' miliseconds.
@@ -34,7 +34,7 @@ def ping_host(ip):
     #Get the reponse time of the ping request by taking the time after the ping finished and subtracting it from the time before the ping request and then multiple it by 1000 to get the milliseconds and round it 2 decimal places.
     response_time = round((time.time() - start_time) * 1000, 2)
 
-    #For these if statements if the result is 0 then we know the IP is UP, if we have a standard error then we return the standard error we got and strip any unnecessary information.
+    #For these if statements if the return result is 0 then we know the IP is "UP", if we have a standard error then we return the standard error we got and strip any unnecessary information.
     #Finally if the standard result has something like timed out in it then we know it is down and without giving us all the extra information if the standard output says 
     #"requested timed out" or "timed out" then we return "DOWN" plus 'No response".
     if result.returncode == 0:
@@ -84,7 +84,11 @@ def scan_network(cidr):
     print(f"\nScan complete. Found {up_count} active hosts, {down_count} down, {error_count} errors")
 
 if __name__ == "__main__":
+    
     parser = argparse.ArgumentParser(description="Scan IP addresses within a CIDR range.")
+    
     parser.add_argument("cidr", help="The CIDR notation of the network to scan")
+    
     args = parser.parse_args()
+    
     scan_network(args.cidr)
